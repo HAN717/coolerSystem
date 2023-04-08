a04121f39680395f62ce099c390dd7c238735331
好听的名字 <2508609654@qq.com>
2023-04-08 21:27:55 +0800

    demo完成

--- src/main.js
+++ src/main.js
@@ -1,3 +1,6 @@
 import { createApp } from 'vue'
+import * as echarts from 'echarts'
+const app = createApp(App);
+app.config.globalProperties.$echarts = echarts
 import App from './App.vue'
 createApp(App).mount('#app')
--- src/views/home/home.vue
+++ src/views/home/home.vue
@@ -6,9 +6,19 @@
         </div>
     </header>
     <div class="main">
-         <!-- content -->
-         <div class="total">
-            <canvas class="rain"></canvas>
+        <!-- content -->
+        <!-- left -->
+        <div id="left">
+            <!-- 入风口 -->
+            <div id="enterPart">
+                <div>冷却机入口处</div>
+                <div id="enterPart1" style="width: 23.4vw; height: 35vh"></div>
+                <div id="enterPart2" style="width: 23.4vw; height: 35vh"></div>
+            </div>
+        </div>
+        <!-- center -->
+        <div class="total">
+             <canvas class="rain"></canvas>
             <div class="sphere">
                 <div class="sphere-bg"></div>
                 <div class="sum">
@@ -38,6 +48,15 @@
                 <p>湿度监测</p>
             </div>
         </div>
+         <!-- right -->
+         <div id="right">
+            <!-- 出风口 -->
+            <div id="exitPart">
+                <div>冷却机出口处</div>
+                <div id="exitPart1" style="width: 23.4vw; height: 35vh"></div>
+                <div id="exitPart2" style="width: 23.4vw; height: 35vh"></div>
+            </div>
+        </div>
     </div>
      <!-- 底部模块 -->
      <div class="bottom">
@@ -48,6 +67,108 @@
 <script>
 import "./main.css";
 import "../../assets/js/main";
+import {onMounted} from "vue";
+import * as echarts from 'echarts';
+export default {
+  data() {
+    return {
+
+    }
+  },
+  setup() {
+    onMounted(() => {//需要获取到element,所以是onMounted的Hook
+
+    // 左侧part1
+      let myChartL1 = echarts.init(document.getElementById("enterPart1"));
+      // 绘制图表
+      myChartL1.setOption({
+        title: { text: "送风温度" },
+        tooltip: {},
+        xAxis: {
+          data: ["12:05", "12:06", "12:07", "12:08", "12:09", "12:10"],
+        },
+        yAxis: {},
+        series: [
+          {
+            name: "tem",
+            type: "line",
+            data: [36, 35, 34, 35, 33, 32],
+          },
+        ],
+      });
+      window.onresize = function () {//自适应大小
+        myChartL1.resize();
+      };
+
+    // 左侧part2  
+    let myChartL2 = echarts.init(document.getElementById("enterPart2")); 
+      // 绘制图表
+      myChartL2.setOption({
+        title: { text: "送风湿度" },
+        tooltip: {},
+        xAxis: {
+          data: ["12:05", "12:06", "12:07", "12:08", "12:09", "12:10"],
+        },
+        yAxis: {},
+        series: [
+          {
+            name: "tem",
+            type: "line",
+            data: [23, 35, 45, 55, 66, 77],
+          },
+        ],
+      });
+      window.onresize = function () {//自适应大小
+        myChartL2.resize();
+      };
+
+    // 右侧part1
+    let myChartR1 = echarts.init(document.getElementById("exitPart1"));
+      // 绘制图表
+      myChartR1.setOption({
+        title: { text: "送风温度" },
+        tooltip: {},
+        xAxis: {
+          data: ["12:05", "12:06", "12:07", "12:08", "12:09", "12:10"],
+        },
+        yAxis: {},
+        series: [
+          {
+            name: "tem",
+            type: "line",
+            data: [36, 35, 34, 35, 33, 32],
+          },
+        ],
+      });
+      window.onresize = function () {//自适应大小
+        myChartR1.resize();
+      };
+
+    // 右侧part2  
+    let myChartR2 = echarts.init(document.getElementById("exitPart2")); 
+      // 绘制图表
+      myChartR2.setOption({
+        title: { text: "送风湿度" },
+        tooltip: {},
+        xAxis: {
+          data: ["12:05", "12:06", "12:07", "12:08", "12:09", "12:10"],
+        },
+        yAxis: {},
+        series: [
+          {
+            name: "tem",
+            type: "line",
+            data: [23, 35, 45, 55, 66, 77],
+          },
+        ],
+      });
+      window.onresize = function () {//自适应大小
+        myChartR2.resize();
+      };
+
+    });
+  }
+}
 </script>
 <style>
 </style>
